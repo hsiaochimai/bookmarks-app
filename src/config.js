78890053bfceb8f29c02336a90563c945d290428
Jennifer Hsiao <jchmai21@gmail.com>
2019-04-13 14:29:08 -0700

    some more committs

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$e58RTq3jASv1OL73kGqkHu5CIhkZY7t1Q0GgAKLKrXB1FZuJyd8FK'
+  ,
 }
